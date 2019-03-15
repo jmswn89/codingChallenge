e4694abec0b0c9a57c1a63d3ac8c8d68b5075355
James Jayaputera <james.jayaputera@gmail.com>
2019-03-15 13:59:08 +1100

    Removed some files due to renaming

--- src/main/java/com/james/tabcorp/codingChallenge/model/repository/TabcorpEntityRepository.java
+++
@@ -1,8 +0,0 @@
-package com.james.tabcorp.codingChallenge.model.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.james.tabcorp.codingChallenge.model.entity.TabcorpEntity;
-
-public interface TabcorpEntityRepository extends CrudRepository<TabcorpEntity, String>{
-
-}
